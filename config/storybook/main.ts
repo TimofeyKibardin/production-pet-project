2eef9d009e90338edb8c70560d7e630802951c2b
TimofeyKibardin <timoshka.kartoshka99@gmail.com>
2025-11-27 20:39:10 +0300

    Added storybook

---
+++ config/storybook/main.ts
@@ -0,0 +1,17 @@
+import type { StorybookConfig } from "@storybook/react-webpack5";
+
+const config: StorybookConfig = {
+    stories: ["../../src/**/*.stories.@(ts|tsx|js|jsx|tsx)"],
+
+    addons: [
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+    ],
+
+    framework: {
+        name: "@storybook/react-webpack5",
+        options: {},
+    }
+};
+
+export default config;
